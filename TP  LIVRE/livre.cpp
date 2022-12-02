91b2be0a55aae02a26a0b1f3cd082aa7f1bccc80
arnaud <arnaud.binet.44@gmail.com>
2022-12-02 17:13:33 +0100

    3 eme commit création de livre

--- TP  LIVRE/livre.cpp
+++ TP  LIVRE/livre.cpp
@@ -1,1 +1,31 @@
 #include "livre.h"
+
+std::string livre::titre()
+{
+	return _titre ;
+}
+
+std::string livre::nomAuteur()
+{
+	return _NomAuteur;
+}
+
+std::string livre::prenomAuteur()
+{
+	return _PrenomAuteur;
+}
+
+std::string livre::genre()
+{
+	return _genre;
+}
+
+int livre::ISBN()
+{
+	return _ISBN;
+}
+
+std::string livre::langue()
+{
+	return _langue;
+}
--- TP  LIVRE/livre.h
+++ TP  LIVRE/livre.h
@@ -1,6 +1,27 @@
 #pragma once
 #include <iostream>
+#include <string>
+#include "Date.h"
+#include <vector>
+
 class livre
 {
+public:
+	std::vector<std::string> ListeIdentifiant;
+	std::string titre();
+	std::string nomAuteur();
+	std::string prenomAuteur();
+	std::string genre();
+	int ISBN();
+	std::string langue();
+
+private:
+
+	std::string _titre;
+	std::string _NomAuteur;
+	std::string _PrenomAuteur;
+	std::string _genre;
+	std::string _langue;
+	int _ISBN;
 };
 
